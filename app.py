a351699fe1862fbf39ac12f85cb96b05996a1b41
PiyushBhandariShorthillsAI <piyush@shorthills.ai>
2024-09-11 17:17:33 +0530

    second commit

--- app.py
+++ app.py
@@ -10,3 +10,13 @@ while count <= n:
     num1, num2 = num2, next_number
     next_number = num1 + num2
 print()
+print("hi")
+
+def factorial(n):
+    if n == 0 or n == 1:
+        return 1
+    else:
+        return n * factorial(n - 1)
+
+number = 5
+print(f"The factorial of {number} is {factorial(number)}")
